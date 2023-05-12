af0b46c7dc2a7da3b021b77abaad8a28cbfe89b1
Ronak Panchal <75179003+ronakpanchalk9@users.noreply.github.com>
2023-05-12 23:43:54 +0530

    Add files via upload

---
+++ 11. Greedy Algorithm/insert_interval.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class insert_interval {
+    static int[][] insertNew(int n, int[][] intervals, int[] newInterval){
+        List<int[]> result = new ArrayList<>();
+        
+        // Iterate through all slots
+        for(int[] slot : intervals)
+        {
+            
+            // if newInterval before slot insert newInterval & update slot as new interval
+            if(newInterval[1] < slot[0])
+            {
+                result.add(newInterval);
+                newInterval = slot;
+            } 
+            
+            // if slot is lesser than new Interval insert slot
+            else if(slot[1] < newInterval[0])
+            {
+                result.add(slot);
+            } 
+            
+            // if above conditions fail its an overlap since possibility of new interval existing in left & right of slot is checked
+            // update lowest of start & highest of end & not insert
+            else {
+                newInterval[0] = Math.min(newInterval[0],slot[0]);
+                newInterval[1] = Math.max(newInterval[1],slot[1]);
+            }
+        }
+        
+        // insert the last newInterval
+        result.add(newInterval);
+        
+        // convert to int[][] array
+        return result.toArray(new int[result.size()][]);
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {{1,2},{3,5},{6,7},{8,10},{12,16}};
+        int[] newEvent = {5,10};
+
+        for (int i = 0; i < intervals.length; i++) {
+            System.out.println(intervals[i][0]+" "+intervals[i][1]);
+        }
+    }
+}
